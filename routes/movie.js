b0b4133a392dc86b4bc7c652183dd38681f21a16
Saitcan TAKILAN <sait@kodanka.com>
2018-08-05 22:23:51 +0300

    /api/movies [GET] edit

--- routes/movie.js
+++ routes/movie.js
@@ -6,7 +6,19 @@ const Movie = require('../models/Movie');
 
 //All movies list
 router.get('/',(req,res) => {
-  const promise = Movie.find({ });
+  const promise = Movie.aggregate([
+    {
+      $lookup: {
+        from: 'directors',
+        localField: 'director_id',
+        foreignField: '_id',
+        as : 'director'
+      }
+    },
+    {
+      $unwind:'$director'      
+    }
+  ]);
 
   promise.then((data)=>{
     res.json(data);

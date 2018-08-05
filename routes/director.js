145ad7080392ede39dd038131c86a381d6d432ee
Saitcan TAKILAN <sait@kodanka.com>
2018-08-05 20:48:58 +0300

    /api/directors POST Method

---
+++ routes/director.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+//Models
+const Director = require('../models/Director');
+
+//Director Add
+router.post('/', (req, res, next) => {
+
+  const director = new Director(req.body);
+  const promise = director.save();
+
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+  
+});
+
+
+module.exports = router;

c6983e80bff19f0198191174b4cc8d8035d24351
Saitcan TAKILAN <sait@kodanka.com>
2018-08-06 12:02:11 +0300

    bcrypt password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+
 //Models
 const User = require('../models/User');
 
@@ -12,18 +14,19 @@ router.get('/', (req, res, next) => {
 /* POST register page. */
 router.post('/register', (req, res, next) => {
   const { username, password } = req.body;
-  const user = new User({
-    username,
-    password
-  });
-
-  const promise = user.save();
-  promise.then((data)=>{
-    res.json(data);
-  }).catch((err)=>{
-    res.json(err);
-  });
-
-
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+  
+    const promise = user.save();
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+  }); 
+ 
 });
 module.exports = router;

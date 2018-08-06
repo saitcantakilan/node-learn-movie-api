19bf7c770e4ba9b13800e19d4f5e740004a94589
Saitcan TAKILAN <sait@kodanka.com>
2018-08-06 14:30:14 +0300

    describe('/GET/:movie_id movie', () => {

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -60,4 +60,24 @@ describe('/api/movies tests', () => {
 		});
 	});
 
+	describe('/GET/:movie_id movie', () => {
+		it('it should GET a movie by the given id', (done) => {
+			chai.request(server)
+				.get('/api/movies/'+ movieId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+					res.body.should.have.property('_id').eql(movieId);
+					done();
+				});
+		});
+	});
+
 });

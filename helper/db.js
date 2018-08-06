34843af43f3ec30ac25e59640711fa19158cd2ef
Saitcan TAKILAN <sait@kodanka.com>
2018-08-06 14:26:11 +0300

    get all movies tests

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports= () => {
     mongoose.connect('mongodb://movie_user:abcd1234@ds111192.mlab.com:11192/movie-api',{ useNewUrlParser: true });
     mongoose.connection.on('open',() =>{
-        console.log("MongoDB Bağlantısı Sağlandı");
+        //console.log("MongoDB Bağlantısı Sağlandı");
     });
     mongoose.connection.on('error',(err) =>{
         console.log("MongoDB : ERROR!", err);
---
+++ test/api/movie.test.js
@@ -0,0 +1,64 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../../app');
+chai.use(chaiHttp);
+
+let token, movieId;
+
+describe('/api/movies tests', () => {
+	before((done) => {
+		chai.request(server)
+			.post('/authenticate')
+			.send({ username: 'test', password: 'asd123' })
+			.end((err,res) => {
+				token = res.body.token;
+				done();
+			});
+	});
+
+	describe('/GET movies', () => {
+		it('it should GET all the movies', (done) => {
+			chai.request(server)
+				.get('/api/movies')
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
+		})
+	});
+
+	describe('/POST movie', () => {
+		it('it should POST a movie', (done) => {
+			const movie = {
+				title: 'Test Movie',
+				director_id: '5b6737e1a2d1222864f7b5e4',
+				category: 'Komedi',
+				country: 'Türkiye',
+				year: 1950,
+				imdb_score: 8
+			};
+
+			chai.request(server)
+				.post('/api/movies')
+				.send(movie)
+				.set('x-access-token', token)
+				.end((err, res) =>{
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+					movieId = res.body._id;
+					done();
+				});
+		});
+	});
+
+
+});
